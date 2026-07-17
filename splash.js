9fd3dad8bbbb4da72676d1c4732658f53f1dfbf8
Melo-art-coder <MelodyKamto@gmail.com>
2026-07-17 18:00:42 +0100

    Create splash.js

---
+++ splash.js
@@ -0,0 +1,15 @@
+// =====================================
+// MELOSAV SPLASH SCREEN
+// =====================================
+
+// Wait for the splash animation
+window.addEventListener("load", () => {
+
+    // Automatically open Welcome Screen
+    setTimeout(() => {
+
+        window.location.href = "welcome.html";
+
+    }, 3000);
+
+});

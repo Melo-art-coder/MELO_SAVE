51a61520e7161e284fad0a1fd54d939f0f42abca
Melo-art-coder <MelodyKamto@gmail.com>
2026-07-18 22:29:43 +0100

    Create wallet.js

---
+++ wallet.js
@@ -0,0 +1,33 @@
+/* =====================================
+   MELOSAV WALLET V1
+===================================== */
+
+console.log("WALLET V1 LOADED");
+
+function updateForeignWallets(){
+
+    const user = JSON.parse(
+        localStorage.getItem("meloCurrentUser")
+    );
+
+    if(!user) return;
+
+    const balance =
+        Number(user.balance || 0);
+
+    document.getElementById("usdBalance").textContent =
+        formatUSD(
+            nairaToUSD(balance)
+        );
+
+    document.getElementById("eurBalance").textContent =
+        formatEUR(
+            nairaToEUR(balance)
+        );
+
+    document.getElementById("gbpBalance").textContent =
+        formatGBP(
+            nairaToGBP(balance)
+        );
+
+}

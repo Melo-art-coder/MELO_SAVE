7d0ac23958d2e90dac66d624f8ac523e90060f3c
Melo-art-coder <MelodyKamto@gmail.com>
2026-07-18 23:08:31 +0100

    Create pwa.js

---
+++ pwa.js
@@ -0,0 +1,24 @@
+/* =====================================
+   MELOSAV PWA
+===================================== */
+
+if ("serviceWorker" in navigator) {
+
+    window.addEventListener("load", () => {
+
+        navigator.serviceWorker
+        .register("sw.js")
+        .then(() => {
+
+            console.log("✅ Service Worker Registered");
+
+        })
+        .catch(error => {
+
+            console.log(error);
+
+        });
+
+    });
+
+}
